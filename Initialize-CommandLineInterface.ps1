a8b613dd3e2a931e769a6855fafb9211c534ab2a
Drewry Pope <drewrypope@gmail.com>
2023-11-05 22:20:06 -0600

    if not

--- Initialize-CommandLineInterface.ps1
+++ Initialize-CommandLineInterface.ps1
@@ -1,5 +1,11 @@
 Set-StrictMode -Version Latest
-Invoke-RestMethod get.scoop.sh | Invoke-Expression
+
+if (-not (Get-Command 'scoop' -ErrorAction SilentlyContinue)) {
+    Invoke-RestMethod get.scoop.sh | Invoke-Expression
+} else {
+    Write-Verbose "Scoop already installed." -Verbose
+}
 scoop install aria2
-scoop bucket known | % { scoop bucket add $_ }
+scoop bucket known | ForEach-Object { scoop bucket add $_ }
+scoop update
 scoop --version
