c1dbf9130db68b17769441f99ec84e4ae3861d22
Drewry Pope <drewrypope@gmail.com>
2023-11-05 22:52:10 -0600

    setup

--- Initialize-CommandLineInterface.ps1
+++ Initialize-CommandLineInterface.ps1
@@ -8,7 +8,11 @@ if (-not (Get-Command 'scoop' -ErrorAction SilentlyContinue)) {
 
 scoop install aria2
 
+scoop config aria2-enabled true
+
 scoop bucket known | ForEach-Object { scoop bucket add $_ }
+
+$currentBuckets = scoop bucket list | ForEach-Object { $_.Name }
 
 $buckets = Get-Content .\buckets.json | ConvertFrom-Json
 
@@ -16,9 +20,13 @@ foreach ($property in $buckets.PSObject.Properties) {
     $name = $property.Name
     $url = $property.Value
 
+    if ($currentBuckets -contains $name) {
+        Write-Verbose -Verbose "Bucket $name already added. Removing..."
+        scoop bucket rm $name
+    }
     scoop bucket add $name $url
 }
 
 scoop update
-
 scoop --version
+scoop status
