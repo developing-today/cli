867032d3f54319e9fa977a23ca7a5e11f30b81bf
Drewry Pope <drewrypope@gmail.com>
2023-11-05 22:34:49 -0600

    improve

--- Initialize-CommandLineInterface.ps1
+++ Initialize-CommandLineInterface.ps1
@@ -5,7 +5,20 @@ if (-not (Get-Command 'scoop' -ErrorAction SilentlyContinue)) {
 } else {
     Write-Verbose -Verbose "Scoop already installed."
 }
+
 scoop install aria2
+
 scoop bucket known | ForEach-Object { scoop bucket add $_ }
+
+$buckets = Get-Content .\buckets.json | ConvertFrom-Json
+
+foreach ($property in $buckets.PSObject.Properties) {
+    $name = $property.Name
+    $url = $property.Value
+
+    scoop bucket add $name $url
+}
+
 scoop update
+
 scoop --version
